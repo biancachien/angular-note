1deecb1ad00a86ea7bc7843f57daf96063a0d704
Bianca Chien <cocoamacaron@gmail.com>
2021-06-20 23:50:22 +0800

    update

--- src/app/article/article-body/article-body.component.ts
+++ src/app/article/article-body/article-body.component.ts
@@ -1,18 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-body',
   templateUrl: './article-body.component.html',
-  styleUrls: ['./article-body.component.scss']
+  styleUrls: ['./article-body.component.scss'],
 })
-export class ArticleBodyComponent implements OnInit {
+export class ArticleBodyComponent implements OnInit, OnChanges {
   // 將item從父元件input進來
   @Input()
   item: any;
 
-  constructor() { }
+  @Input()
+  counter: any;
 
+  constructor() {
+    console.log('ArticleBodyComponent: constuctor');
+  }
   ngOnInit(): void {
+    console.log('ArticleBodyComponent ' + this.item.id + ': ngOnInit');
   }
 
+  ngOnChanges(changes) {
+    console.log('ArticleBodyComponent' + this.item.id + ': ngOnChanges');
+    console.log(changes);
+  }
 }
--- src/app/article/article-header/article-header.component.ts
+++ src/app/article/article-header/article-header.component.ts
@@ -1,14 +1,24 @@
-import { Component, EventEmitter, Input, OnInit, Output, OnDestroy } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  OnDestroy,
+  OnChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-article-header',
   templateUrl: './article-header.component.html',
-  styleUrls: ['./article-header.component.scss']
+  styleUrls: ['./article-header.component.scss'],
 })
-export class ArticleHeaderComponent implements OnInit {
+export class ArticleHeaderComponent implements OnInit, OnDestroy, OnChanges {
   // 從父元件(article-list.component)傳資料進子元件
   @Input()
   item: any;
+
+  orig_item: any;
 
   // 把資料從子元件送回父元件(article-list.component)
   @Output()
@@ -20,21 +30,20 @@ export class ArticleHeaderComponent implements OnInit {
 
   isEdit = false;
   newTitle = '';
-  constructor() { }
+  constructor() {}
 
-  doEdit(title: any) {
-    this.newTitle = title;
-    this.titleChanged.emit({ id: this.item.id, title: title });
-  }
-
-  //往父元件發射做觸發刪除文章的動作(this.item等於父元件的$event資料)
-  deleteArticle() {
-    this.delete.emit(this.item);
-  }
-
-  //angular生命週期(ㄧ)
+  //angular生命週期(一)
   ngOnInit(): void {
-    this.newTitle = this.item.title;
+    // this.newTitle = this.item.title;
+  }
+
+  ngOnChanges(changes): void {
+    if (changes.item) {
+      this.orig_item = changes.item.currentValue;
+      this.item = Object.assign({}, changes.item.currentValue);
+    }
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
   }
 
   //angular生命週期(二)
@@ -42,5 +51,21 @@ export class ArticleHeaderComponent implements OnInit {
     //Called once, before the instance is destroyed.
     //Add 'implements OnDestroy' to the class.
     //很少用
+  }
+
+  doEdit(title: any) {
+    this.newTitle = title;
+    // emit({ id: this.item.id, title: title })的事件資料(包含id和title)
+    this.titleChanged.emit(this.item.id);
+  }
+
+  doCnacel() {
+    this.item = Object.assign({}, this.orig_item);
+    this.isEdit = false;
+  }
+
+  //往父元件發射做觸發刪除文章的動作(this.item等於父元件的$event資料)
+  deleteArticle() {
+    this.delete.emit(this.item);
   }
 }
